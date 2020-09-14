d643f038fb28acd37448ef08213a4025ed9694e6
robertmiller72 <robert.miller@mu.ie>
2020-09-14 14:17:46 +0100

    Recursive Solutions for 403 Frog Jump

---
+++ 403 - Frog Jump/RecursiveDP.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        
+        target = stones[-1]
+        stones = set(stones)
+        cache = dict()
+        
+        def jump(val, j):
+            if val == target:
+                return True
+            
+            if val not in stones:
+                return False
+            
+            if (val, j) in cache:
+                return cache[(val, j)]
+            
+            if jump(val+j+1, j+1):
+                cache[(val, j)] = True
+                return True
+            
+            if jump(val+j, j):
+                cache[(val, j)] = True
+                return True
+            
+            if j - 1 > 0:
+                cache[(val, j)] = jump(val+j-1, j-1)
+                return cache[(val, j)]
+            
+            cache[(val, j)] = False
+            return False
+        
+        return jump(1, 1)
---
+++ 403 - Frog Jump/RecursiveDP_LruCache.py
@@ -0,0 +1,20 @@
+class Solution:
+    from functools import lru_cache
+    def canCross(self, stones: List[int]) -> bool:
+        
+        target = stones[-1]
+        stones = set(stones)
+        
+        @lru_cache(maxsize=None)
+        def jump(val, j):
+            if val == target:
+                return True
+            
+            if val not in stones:
+                return False
+            
+            if j - 1 > 0:
+                return jump(val+j+1, j+1) or jump(val+j, j) or jump(val+j-1, j-1)
+            return jump(val+j+1, j+1) or jump(val+j, j)
+        
+        return jump(1, 1)
