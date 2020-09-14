4dc38e7a10a812a909084db6f8ae51fe4faf78e5
robertmiller72 <robert.miller@mu.ie>
2020-09-14 13:17:44 +0100

    Iterative Solution to 403 Frog Jump

---
+++ 403 - Frog Jump/IterativeDP.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        tracker = {
+            0: set()
+        }
+        tracker[0].add(1)
+        
+        for i in range(len(stones) - 1):
+            if stones[i] not in tracker:
+                continue
+            for jump in tracker[stones[i]]:
+                reach = stones[i] + jump
+                if reach not in tracker:
+                    tracker[reach] = set()
+                if reach == stones[-1]:
+                    return True
+                tracker[reach].add(jump + 1)
+                tracker[reach].add(jump)
+                if jump - 1 > 0:
+                    tracker[reach].add(jump-1)
+                    
+        return False
